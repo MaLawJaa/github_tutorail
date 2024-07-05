2041dca7bc18e7140be97e123fb0a2cc87d9b278
MaLawJaa <6611850012@mut.ac.th>
2024-07-05 17:13:31 +0700

    Update by github to index.php

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
     echo "<h1>My repository</h1>"
     echo "<h1>My Name is Ozen</h1>"    
     echo "<h1>My Name is MaLawjaa</h1>"    
+    echo "<h1>My Name is PaiLawjaa</h1>"    
+    echo "<h1>My Name is Come</h1>"    
+    echo "<h1>My Name is Sun</h1>"    
     ?>
